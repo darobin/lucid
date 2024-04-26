ec1fe222074ae4a959531e66a8f91f1da5a2f2be
Robin Berjon <robin.berjon@nytimes.com>
2024-04-26 11:53:00 -0400

    watcher works

--- test/watching.js
+++ test/watching.js
@@ -1,6 +1,6 @@
 
 import { equal, deepEqual } from 'node:assert';
-import { mkdtemp, cp } from 'node:fs/promises';
+import { mkdtemp, cp, writeFile, rm } from 'node:fs/promises';
 import { join } from 'node:path';
 import { tmpdir } from 'node:os';
 import makeRel from '../lib/rel.js';
@@ -60,7 +60,55 @@ describe('Watcher', () => {
     await cp(join(fixtures, 'poem.txt'), join(tmpDir, 'poem.txt'));
     return p;
   });
-  // XXX
-  // - change a file
-  // - delete a file
+  it('processes changing a file', async function () {
+    this.timeout(5000);
+    const p = new Promise((resolve, reject) => {
+      w.once('update', (map, type, cid) => {
+        try {
+          equal(type, 'change', 'we see a change');
+          equal(cid, 'bafkr4ib47dntyr4jhcgcbx3wknk7okpidiuikc6x4ulsvurlrzsgxpmjlm', 'correct CID');
+          deepEqual(
+            map, 
+            {
+              bafkr4ib47dntyr4jhcgcbx3wknk7okpidiuikc6x4ulsvurlrzsgxpmjlm: "index.html",
+              bafkr4idcy33utsake6atvbagnojkn7odp7mdo6n7tvspd4ndnewphj67xu: "wtf.jpg",
+              bafkr4idla6d37zoddo4i4wvs5zkuxwvyuczpwjjna6xcx4uj7bpmy3276i: "poem.txt",
+            },
+            'map correct'
+          );
+          resolve();
+        }
+        catch (err) {
+          reject(err);
+        }
+      });
+    });
+    await writeFile(join(tmpDir, 'index.html'), 'This file now intentionally left blank.');
+    return p;
+  });
+  it('processes deleting a file', async function () {
+    this.timeout(5000);
+    const p = new Promise((resolve, reject) => {
+      w.once('update', (map, type, cid) => {
+        try {
+          equal(type, 'delete', 'we see a deletion');
+          equal(cid, 'bafkr4idcy33utsake6atvbagnojkn7odp7mdo6n7tvspd4ndnewphj67xu', 'correct CID that was deleted');
+          deepEqual(
+            map, 
+            {
+              bafkr4ib47dntyr4jhcgcbx3wknk7okpidiuikc6x4ulsvurlrzsgxpmjlm: "index.html",
+              bafkr4idla6d37zoddo4i4wvs5zkuxwvyuczpwjjna6xcx4uj7bpmy3276i: "poem.txt",
+            },
+            'map correct'
+          );
+          resolve();
+        }
+        catch (err) {
+          reject(err);
+        }
+      });
+    });
+    await rm(join(tmpDir, 'wtf.jpg'));
+    return p;
+  });
 });
